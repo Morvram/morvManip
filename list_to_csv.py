44a8cdddfb79aef808f4b7695395f1337af6f87a
JRMhere <joshua6781@gmail.com>
2020-09-01 21:16:22 -0500

    1 Sep 2020

--- list_to_csv.py
+++ list_to_csv.py
@@ -1,6 +1,9 @@
 import csv
-def list_to_csv(z, headers):
-    with open("outfile.csv", "w+", newline="") as outfile:
+def list_to_csv(z, headers, outfile_name):
+    #z = a list to write
+    #headers = the headers!
+    #outfile_name is self-explanatory
+    with open(outfile_name, "w+", newline="") as outfile:
         out_writer = csv.writer(outfile, delimiter=",")
         out_writer.writerow(headers)
         for item in z:
