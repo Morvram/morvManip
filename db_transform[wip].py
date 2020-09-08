85b9109b6f84c71a5efc33cc4a5063881abb6a71
JRMhere <joshua6781@gmail.com>
2020-09-07 20:45:24 -0500

    7 Sep 2020

--- db_transform[wip].py
+++ db_transform[wip].py
@@ -1,2 +1,3 @@
 #TODO take a list of contents and the name of a column A in a database, along with the name of a second column B.
 #Return a list where for each row where the contents of column A are in the list, the contents of column B are added to the list.
+
