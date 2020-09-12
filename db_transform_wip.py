90386f49e815219887ca4094657a221a8d68aeb1
JRMhere <joshua6781@gmail.com>
2020-09-11 19:08:13 -0500

    11 Sep 2020

--- db_transform_wip.py
+++ db_transform_wip.py
@@ -1,3 +1,8 @@
 #TODO take a list of contents and the name of a column A in a database, along with the name of a second column B.
 #Return a list where for each row where the contents of column A are in the list, the contents of column B are added to the list.
 
+import pyodbc
+
+def list_transform(il, na, nb):
+    #il -> a list
+    pass
