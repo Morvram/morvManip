2ff07f1bc1844a271fb5ace36f404e7af6e7de27
Morvram <JRMhere@protonmail.ch>
2023-12-18 13:04:36 -0500

    Create number_updowngrade.py

---
+++ number_updowngrade.py
@@ -0,0 +1,7 @@
+#TODO a script that takes a string, identifies numbers within it, and increases or reduces each number by a specified amount
+
+def number_updowngrade (instr, upby):
+    #INPUT: instr = String in which the numbers will be downgraded. upby = Integer by which to change the numbers.
+    #OUTPUT: New string with all the numbers altered by upby.
+    
+    
