c6d1c3165ebe47f8234f46762b53ec5987fbb5dc
Morvram <JRMhere@protonmail.ch>
2023-12-18 13:31:57 -0500

    number_updowngrade.py working script

--- number_updowngrade.py
+++ number_updowngrade.py
@@ -1,7 +1,23 @@
-#TODO a script that takes a string, identifies numbers within it, and increases or reduces each number by a specified amount
+#a script that takes a string, identifies numbers within it, and increases or reduces each number by a specified amount
+
+import re
 
 def number_updowngrade (instr, upby):
     #INPUT: instr = String in which the numbers will be downgraded. upby = Integer by which to change the numbers.
     #OUTPUT: New string with all the numbers altered by upby.
-    
-    
+
+    #Create list of integers
+    res = [int(i) for i in instr.split() if i.isdigit()]
+
+    #Alter each number in the list
+    res_old = res.copy()
+    for i in range(0, len(res)):
+        res[i] = res[i] + upby
+
+    #replace the numbers in the string
+    res_old = list(set(res_old))
+    res = list(set(res))
+    for i in range(0, len(res)):
+        instr = instr.replace(str(res_old[i]), str(res[i]))
+
+    return instr
