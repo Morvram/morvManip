c5956ba80fcdca41fb5500371a2f044b4bafcb98
Morvram <JRMhere@protonmail.ch>
2024-01-01 17:08:13 -0500

    1st ver: skeleton of iteration setup

--- csvToMARC.py
+++ csvToMARC.py
@@ -13,11 +13,18 @@ def csv_to_marc(filepath):
     outpath = filepath.replace(".csv", ".dat")
         #Later, we can use this .dat file to put the records into.
     
+    #TODO clear the contents of outpath file so that we can write out all the new records without having duplicate records.
+    
     #TODO loop through rows of the csv file for each record. For reference consider "Here's an example of creating a record and writing it out" here: https://pypi.org/project/pymarc/
 
+    for row in infile.iterrows():
+        
 
-    with open(outpath, 'wb') as out: #for each item in the loop
+        #Add to output.
+        with open(outpath, 'wb') as out: #for each item in the loop. TODO double check that 'wb' is the right parameter.
         out.write(record.as_marc())
+
+    
 
     return(outpath)
 
