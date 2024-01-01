6f0c8c5882d96ffd770a69b3c438415b55be68a9
Morvram <JRMhere@protonmail.ch>
2024-01-01 17:00:44 -0500

    give skeleton to csv_to_marc() function.

--- csvToMARC.py
+++ csvToMARC.py
@@ -6,6 +6,19 @@ import pandas as pd
 
 def csv_to_marc(filepath):
     #INPUT filepath to .csv file
-    #OUTPUT filepath to .marc file
+    #OUTPUT filepath to .marc file as a String.
 
     infile = pd.read_csv(filepath)
+
+    outpath = filepath.replace(".csv", ".dat")
+        #Later, we can use this .dat file to put the records into.
+    
+    #TODO loop through rows of the csv file for each record. For reference consider "Here's an example of creating a record and writing it out" here: https://pypi.org/project/pymarc/
+
+
+    with open(outpath, 'wb') as out: #for each item in the loop
+        out.write(record.as_marc())
+
+    return(outpath)
+
+
