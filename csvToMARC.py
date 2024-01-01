1e83694e3dc92360b593f1df7fba46e9d3fc3ae7
Morvram <JRMhere@protonmail.ch>
2024-01-01 17:16:14 -0500

    if AUTHOR in infile

--- csvToMARC.py
+++ csvToMARC.py
@@ -17,8 +17,20 @@ def csv_to_marc(filepath):
     
     #TODO loop through rows of the csv file for each record. For reference consider "Here's an example of creating a record and writing it out" here: https://pypi.org/project/pymarc/
 
-    for row in infile.iterrows():
+    for i in range(0, len(infile)):
+        record = Record()
         
+        #TODO add these if cases for all potential columns that could be in the infile. Check reference on MARC writing (https://www.loc.gov/marc/umb/um01to06.html) to see what the contents of these fields should be.
+        if 'AUTHOR' in infile:
+            record.add_field(
+                Field(
+                    tag = '100',
+                    indicators = ['0', '1'], #A title main entry is involved. TODO create separate function to determine what the indicators should be, given greater knowledge of MARC format and its meanings.
+                    subfields = [
+                        Subfield(code = 'a', value = infile['AUTHOR'][i])
+                    ]
+                )
+            )
 
         #Add to output.
         with open(outpath, 'wb') as out: #for each item in the loop. TODO double check that 'wb' is the right parameter.
@@ -27,5 +39,4 @@ def csv_to_marc(filepath):
     
 
     return(outpath)
-
 
