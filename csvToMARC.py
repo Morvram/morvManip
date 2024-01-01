828097318224ebbd4db40c8d50a4c7a1065ab9e8
Morvram <JRMhere@protonmail.ch>
2024-01-01 16:31:16 -0500

    let pandas import .csv file to the data space

--- csvToMARC.py
+++ csvToMARC.py
@@ -1,4 +1,11 @@
 #TODO Outputs a MARC file with fields entered depending on what fields exist in the CSV file.
 
 import pymarc
+import rpy2
+import pandas as pd
 
+def csv_to_marc(filepath):
+    #INPUT filepath to .csv file
+    #OUTPUT filepath to .marc file
+
+    infile = pd.read_csv(filepath)
