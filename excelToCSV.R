38de7f514a26cdf33eee33e708c210bd2184c283
Morvram <JRMhere@protonmail.ch>
2024-01-01 12:49:34 -0500

    excelToCSV R function (helper function for csvToMARC)

--- excelToCSV.R
+++ excelToCSV.R
@@ -1,2 +1,15 @@
-#TODO excel to CSV for MARC
+#excel to CSV for MARC
+
+library(readxl)
+
+excel_to_csv <- function(input_path, sheetname=1) {
+    #INPUT: input_path is location of excel file to read in.
+        #sheetname can be a string, or it can be the default of 1. #TOTEST do default parameters work like this in R?
+    #TODO OUTPUT: filepath to a newly created .csv file.
+
+    infile <- read_excel(input_path, sheet = sheetname)
+    #TODO any necessary manipulation goes here.
+    write.csv(infile, str_replace(".xlsx", ".csv"))
+    return(str_replace(".xlsx", ".csv"))
+}
 
